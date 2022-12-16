e87647d020e67365bdac6c49f2087bfc0bb5272a
Hiệp Nguyễn <104041572+nguyenhiep19970302@users.noreply.github.com>
2022-12-16 17:43:23 +0700

    Update Validation.js

--- adminPhoneShop/app/models/Validation.js
+++ adminPhoneShop/app/models/Validation.js
@@ -1,26 +1,26 @@
-// const getEle = (id) => document.getElementById(id);
-// class Validation {
-//     checkOtion = (idSel, errId, mess) => {
-//         if (getEle(idSel).selectedIndex !== 0) {
-//             getEle(errId).innerHTML = "";
-//             getEle(errId).style.display = "none";
-//             return true;
-//         }
-//         getEle(errId).innerHTML = mess;
-//         getEle(errId).style.display = "block";
-//         return false;
-//     }
+const getEle = (id) => document.getElementById(id);
+class Validation {
+    checkOtion = (idSel, errId, mess) => {
+        if (getEle(idSel).selectedIndex !== 0) {
+            getEle(errId).innerHTML = "";
+            getEle(errId).style.display = "none";
+            return true;
+        }
+        getEle(errId).innerHTML = mess;
+        getEle(errId).style.display = "block";
+        return false;
+    }
 
-//     checkNull = (val, errId, mess) => {
-//         if (val === "") {
-//             getEle(errId).innerHTML = mess;
-//             getEle(errId).style.display = "block";
-//             return false;
-//         }
-//         getEle(errId).innerHTML = "";
-//         getEle(errId).style.display = "none";
-//         return true;
-//     }
-// }
+    checkNull = (val, errId, mess) => {
+        if (val === "") {
+            getEle(errId).innerHTML = mess;
+            getEle(errId).style.display = "block";
+            return false;
+        }
+        getEle(errId).innerHTML = "";
+        getEle(errId).style.display = "none";
+        return true;
+    }
+}
 
-// export default Validation;
+export default Validation;
